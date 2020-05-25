c74b9d14f7477005738e419b302b1cad086e99a0
farabbi <jz3593@nyu.edu>
2020-05-24 20:29:56 -0400

    continue fix nav.js

--- nav.js
+++ nav.js
@@ -1,22 +1,32 @@
 var path = window.location.pathname;
-console.log(path);
 var categories = ["01_short_films", "02_games", "03_physical_computing", "04_photography"];
 for (var i = 0; i < categories.length; i++) {
     if (path.indexOf(categories[i]) > -1) {
         break;
     }
 }
-console.log(i);
 $(document).ready(function() {
     $("#nav-placeholder").load("/index.html #navigation");
     $(".subnav").hide();
-    if (i == 0) {
-        $(".short_films").show();
-    } else if (i == 1) {
-        $(".games").show();
-    } else if (i == 2) {
-        $(".physical_computing").show();
-    } else if (i == 3) {
-        $(".photography").show();
+    console.log(i);
+    switch (i) {
+        case 0:
+            $(".short_films").show();
+            console.log("short_films");
+            break;
+        case 1:
+            $(".games").show();
+            console.log("games");
+            break;
+        case 2:
+            $(".physical_computing").show();
+            console.log("physical_computing");
+            break;
+        case 3:
+            $(".photography").show();
+            console.log("photography");
+            break;
+        default:
+            break;
     }
 });
