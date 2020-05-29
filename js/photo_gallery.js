6c827e7a93a415a206afd019b32805c6fb8987ff
farabbi <jz3593@nyu.edu>
2020-05-29 09:39:59 -0400

    finish

--- js/photo_gallery.js
+++ js/photo_gallery.js
@@ -8,29 +8,41 @@ for (var i = 0; i < photos.length; i++) {
 }
 var selectors = $("#photoGallery .selector");
 $(selectors[pho_i]).addClass("selected");
-
-
+var titles = $("#photoGallery .imageTitle");
+titles.hide();
+$(titles[pho_i]).fadeIn();
+var infos = $("#photoGallery .imageInfo");
+infos.hide();
+$(infos[pho_i]).fadeIn();
 
 var prev_photo = function() {
     $(photos[pho_i]).hide();
     $(selectors[pho_i]).removeClass("selected");
+    $(titles[pho_i]).hide();
+    $(infos[pho_i]).hide();
     pho_i--;
     if (pho_i < 0) {
         pho_i += photos.length;
     }
     $(photos[pho_i]).fadeIn();
     $(selectors[pho_i]).addClass("selected");
+    $(titles[pho_i]).fadeIn();
+    $(infos[pho_i]).fadeIn();
 };
 
 var next_photo = function() {
     $(photos[pho_i]).hide();
     $(selectors[pho_i]).removeClass("selected");
+    $(titles[pho_i]).hide();
+    $(infos[pho_i]).hide();
     pho_i++;
     if (pho_i >= photos.length) {
         pho_i -= photos.length;
     }
     $(photos[pho_i]).fadeIn();
     $(selectors[pho_i]).addClass("selected");
+    $(titles[pho_i]).fadeIn();
+    $(infos[pho_i]).fadeIn();
 };
 
 var auto_display = window.setInterval(next_photo, 5000);
